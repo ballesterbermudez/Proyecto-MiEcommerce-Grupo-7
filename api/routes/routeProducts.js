b10c6486eddde5b48c974ca2750a19fe36aa034d
Juan Ventura <juancaven1988@gmail.com>
2022-09-12 12:17:56 -0300

    Rutas de products agregadas

--- api/routes/routeProducts.js
+++ api/routes/routeProducts.js
@@ -1,1 +1,14 @@
-module.exports = ''
+const express = require('express');
+const route = express.Router();
+
+
+route.get('/', ()=>{});
+route.get('/:id', ()=>{});
+route.post('/', ()=>{});
+route.put('/:id', ()=>{});
+route.get('/search', ()=>{});
+route.get('/mostwanted', ()=>{});
+route.delete('/:id', ()=>{});
+
+
+module.exports = route
