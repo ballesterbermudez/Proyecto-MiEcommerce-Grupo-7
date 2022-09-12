90b362b8784056dad13c9840f42e333055efb30e
ballesterbermudez <ballester.bermudez@gmail.com>
2022-09-12 11:50:58 -0300

    Primer commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const routeCarts = require('./api/routes/routeCarts');
+const routeLogin = require('./api/routes/routeLogin');
+const routePictures = require('./api/routes/routePictures');
+const routeProducts = require('./api/routes/routeProducts');
+const routeUsers = require('./api/routes/routeUsers');
+require('dotenv').config();
+
+app.use(express.json());
+
+app.get('/api/v1', (req, res) => {
+    res.status(200).json('Bienvenido al inicio');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Se abrio correctamente en el puerto ' + process.env.PORT);
+})
+
+app.get('*',(req, res) => {
+    res.redirect('/api/v1');
+});
