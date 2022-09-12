7b96ab8509d2b99b1e38273b30588db9e038d513
federicojsd <federicojaviersierra@192.168.1.7>
2022-09-12 14:46:22 -0300

    Push listar usuario por id

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,22 +1,56 @@
-const fs = require('fs');
-const path = require('path');
-const userDirectory = path.resolve(__dirname, "..","data", "users.json");
+const fs = require("fs");
+const path = require("path");
+
+//FUNCIONES POR SI QUERES SACARLAS A OTRO ARCHIVO
+const readUserDB = (dataFile) => {
+  const userDirectory = path.resolve(__dirname, "..", "data", dataFile);
+  return JSON.parse(fs.readFileSync(userDirectory, "utf-8"));
+};
+const findByIdDB = (dataFile, id) => {
+  const userDirectory = path.resolve(__dirname, "..", "data", dataFile);
+  const data = JSON.parse(fs.readFileSync(userDirectory, "utf-8"));
+  return data.find((ele) => ele.id === Number(id));
+};
 
 const usersController = {
   listUsers: (req, res) => {
     try {
-        const users = JSON.parse(fs.readFileSync(userDirectory, 'utf-8'));
-        res.status(200).json({
-            ok: true,
-            msg: 'Lista de usuarios obtenida correctamente',
-            users: users
-        })
+      const users = readUserDB("users.json");
+      res.status(200).json({
+        ok: true,
+        msg: "Lista de usuarios obtenida correctamente",
+        users: users,
+      });
     } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Error al leer la base de datos'
-        })
+      console.log(error);
+      res.status(500).json({
+        ok: false,
+        msg: "Error al leer la base de datos",
+      });
+    }
+  },
+  findUserById: (req, res) => {
+    try {
+      const user = findByIdDB("users.json", req.params.userId);
+      if (user) {
+        res.status(200).json({
+          ok: true,
+          msg: "Usuario obtenido correctamente",
+          users: user,
+        });
+      } else {
+        res.status(401).json({
+          ok: true,
+          msg: `Usuario con id ${req.params.userId} no existe`,
+          users: user,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        ok: false,
+        msg: "Error al leer la base de datos",
+      });
     }
   },
 };
--- api/routes/routeUsers.js
+++ api/routes/routeUsers.js
@@ -2,6 +2,7 @@ const express = require('express');
 const route = express.Router();
 const usersController = require('../controllers/usersController');
 
-route.get('/', usersController.listUsers)
+route.get('/', usersController.listUsers);
+route.get('/:userId', usersController.findUserById)
 
 module.exports = route;
